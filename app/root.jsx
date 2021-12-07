5eaded25e8ea7fba7526766162a4b5242e3ef9b9
Mohamed Said <themohamedsaid@gmail.com>
2021-12-07 17:02:52 +0200

    wip

--- app/root.jsx
+++ app/root.jsx
@@ -6,7 +6,7 @@ import {
     Outlet,
     Scripts,
     ScrollRestoration,
-    useCatch
+    useCatch, useTransition
 } from "remix";
 
 import tailwindStyles from "./tailwind.css"
@@ -102,6 +102,7 @@ function Document({children, title}) {
 }
 
 function Layout({children}) {
+    let transition = useTransition();
     return (
         <>
             <div className="bg-white">
@@ -131,7 +132,44 @@ function Layout({children}) {
             </div>
 
             <div className="max-w-screen-lg mx-auto p-4 sm:p-7 mt-10">
-                {children}
+                {transition.state == 'loading'
+                    ? <div className={'text-center'}>
+                        {/*By Sam Herbert (@sherb), for everyone. More @ http://goo.gl/7AJzbL*/}
+                        <svg width="120" height="30" viewBox="0 0 120 30" xmlns="http://www.w3.org/2000/svg" className={'text-purple-700 fill-current inline'}>
+                            <circle cx="15" cy="15" r="15">
+                                <animate attributeName="r" from="15" to="15"
+                                         begin="0s" dur="0.8s"
+                                         values="15;9;15" calcMode="linear"
+                                         repeatCount="indefinite" />
+                                <animate attributeName="fillOpacity" from="1" to="1"
+                                         begin="0s" dur="0.8s"
+                                         values="1;.5;1" calcMode="linear"
+                                         repeatCount="indefinite" />
+                            </circle>
+                            <circle cx="60" cy="15" r="9" fillOpacity="0.3">
+                                <animate attributeName="r" from="9" to="9"
+                                         begin="0s" dur="0.8s"
+                                         values="9;15;9" calcMode="linear"
+                                         repeatCount="indefinite" />
+                                <animate attributeName="fillOpacity" from="0.5" to="0.5"
+                                         begin="0s" dur="0.8s"
+                                         values=".5;1;.5" calcMode="linear"
+                                         repeatCount="indefinite" />
+                            </circle>
+                            <circle cx="105" cy="15" r="15">
+                                <animate attributeName="r" from="15" to="15"
+                                         begin="0s" dur="0.8s"
+                                         values="15;9;15" calcMode="linear"
+                                         repeatCount="indefinite" />
+                                <animate attributeName="fillOpacity" from="1" to="1"
+                                         begin="0s" dur="0.8s"
+                                         values="1;.5;1" calcMode="linear"
+                                         repeatCount="indefinite" />
+                            </circle>
+                        </svg>
+                    </div>
+                    : children
+                }
             </div>
         </>
     )
